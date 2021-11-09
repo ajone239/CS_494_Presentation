353ed40d5782d7cc23ce595f93e0473becfd8ed1
Austin Jones <ajone239@vols.utk.edu>
2021-11-09 00:09:09 -0500

    started report and added comments

--- DigitStringDiv1.cpp
+++ DigitStringDiv1.cpp
@@ -1,14 +1,20 @@
-#include <string>
-#include <cstring>
-#include <vector>
-#include <list>
-#include <cmath>
+/*
+ * Author: Austin Jones (ajone239)
+ * File: DigitStringDiv1.cpp
+ * Date: 11/08/2021
+ *
+ * Purpose: This file implements the DigitStringDiv1 class. This class carries
+ *          the method that counts all substrings of a digit S which are
+ *          larger than a digit X.
+ */
+
 #include <algorithm>
-#include <map>
-#include <set>
-#include <iostream>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -19,9 +25,11 @@ class DigitStringDiv1 {
     string XS;                                      // X as a string of digits
     int SD;                                         // Number of digits in S
     int XD;                                         // Number of digits in X
-    long long Cache[50][15] = {{-1}};               // Memoization cache for neq_digits()
+    long long Cache[50][15] = {{0}};               // Memoization cache for neq_digits()
 
-    // Return N-choose-K.
+    /*
+     * Return N-choose-K.
+     */
     long long comb(long n, long k) {
       long long rv = 1;
 
@@ -32,12 +40,30 @@ class DigitStringDiv1 {
       return rv;
     }
 
-    // Counting strings whose sizes are > XD.
+    /*
+     * Prints out the whole cache for debugging
+     */
+    void print_cache(void) {
+      for (int i = 0; i < this->SD; i++) {
+        for (int j = 0; j < this->XD; j++) {
+          // Only look at entries that are in the range and used
+          if (this->Cache[i][j]) {
+            printf("(%d, %d) = %lld\n", i, j, this->Cache[i][j]);
+          }
+        }
+      }
+    }
+
+    /*
+     * Counting strings whose sizes are > XD.
+     */
     long long ngtr_digits(int sindex) {
       long long rv = 0;
 
-      // only go to sindex because you want to gurantee it starts at sindex
-      // also use XD because you are doing xd + 1 with using sindex
+      /*
+       * Only go to sindex because you want to gurantee it starts at sindex
+       * Also use XD because you are doing xd + 1 with using sindex
+       */
       for (int i = this->XD; i <= sindex; i++) {
           rv += this->comb(sindex, i);
       }
@@ -45,34 +71,52 @@ class DigitStringDiv1 {
       return rv;
     }
 
-    // Counting strings whose sizes are == XD
+    /*
+     * Counting strings whose sizes are == XD
+     */
     long long neq_digits(int sindex, int xindex) {
 
       long long rv = 0;
 
-      if (xindex >= this->XD) {
+      // Needs valid indexes
+      if (xindex >= this->XD || sindex >= this->SD) {
         return 0;
       }
 
-      if (xindex == this->XD - 1) {
-        if (this->XS[xindex] > this->S[sindex]) {
-          return 1;
-        } else {
-          return 0;
-        }
+      // Cache Hit?
+      if (this->Cache[sindex][xindex]) {
+        return this->Cache[sindex][xindex];
       }
 
-      if (this->XS[xindex] >= this->S[sindex]) {
-        for (int j = 0; j < this->XD; j++) {
+      if (this->S[sindex] > this->XS[xindex]) {
+        /*
+         * If the S digit is greater than X digit then we can skip the rest
+         * of the checks and just count how many strings we can build.
+         */
+        rv = this->comb(
+            this->SD - sindex - 1,
+            this->XD - xindex - 1);
+      } else if (this->S[sindex] == this->XS[xindex]) {
+        // last char in string equal means it isn't greater
+        if (xindex == this->XD - 1) {
+          return 0;
+        }
+        // Recurse down for all substrings
+        for (int j = 1; j < this->SD - sindex; j++) {
           rv += neq_digits(sindex + j, xindex + 1);
         }
       }
+
+      // Fix cache
+      this->Cache[sindex][xindex] = rv;
 
       return rv;
     }
 
   public:
     long long count(string s, int x) {
+
+      // Set up the object
       this->X = x;
       this->S = s;
       this->XS = to_string(x);
@@ -89,9 +133,11 @@ class DigitStringDiv1 {
       // Count Equal
       for (int i = 0; i < this->SD - this->XD + 1; i++) {
         // Start at each index where an equal could start.
-        neq_digits(i, 0);
+        rv += neq_digits(i, 0);
       }
 
+      // Print if you desire
+      // this->print_cache();
       return rv;
     }
 
